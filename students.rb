f29def4d2999da682b9cf6eb0e5e956c6cbc2b1a
Joey Wolf <halfdark000@gmail.com>
2014-04-02 14:16:59 -0700

    started a list of names

---
+++ students.rb
@@ -0,0 +1,20 @@
+Steve Musgrave
+Sroop Sunar
+Colin Marshall
+Josh Fail-Brown
+Louise Lai
+Robin Doble
+Alex Wong
+Scott Dimmock
+Muhanad Al-Rubaiee
+Shelley Hope
+Will Hall
+Oliver Delevingne
+Nico Saueressig
+Apostolis
+Stefania
+Robert Leon
+Emma Williams
+Joey Wolf
+Julie Walker
+Daniel Le Dosquet
